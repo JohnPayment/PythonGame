baf3476a484028551331ef3d26d92fff1d517476
root <root@localhost.localdomain>
2015-04-21 15:30:30 -0700

    Added some Mon stuff.

--- Attacks.py
+++ Attacks.py
@@ -25,21 +25,21 @@ def getAttack(attackNum):
 def CalculateDamage(attack, attacker, defender):
 	damage = 0
 	if attack.offAttr == Attributes.STR:
-		damage = attacker.str * attack.power
+		damage = attacker.attr.STR * attack.power
 	elif attack.offAttr == Attributes.MSTR:
-		damage = attacker.mstr * attack.power
+		damage = attacker.attr.MSTR * attack.power
 	elif attack.offAttr == Attributes.DEF:
-		damage = attacker.DEF * attack.power
+		damage = attacker.attr.DEF * attack.power
 	elif attack.offAttr == Attributes.MDEF:
-		damage = attacker.mdef * attack.power
+		damage = attacker.attr.MDEF * attack.power
 	elif attack.offAttr == Attributes.SPD:
-		damage = attacker.spd * attack.power
+		damage = attacker.attr.SPD * attack.power
 	elif attack.offAttr == Attributes.ACC:
-		damage = attacker.acc * attack.power
+		damage = attacker.ACC * attack.power
 	elif attack.offAttr == Attributes.EVA:
-		damage = attacker.eva * attack.power
+		damage = attacker.EVA * attack.power
 	elif attack.offAttr == Attributes.HP:
-		damage = attacker.hp * attack.power
+		damage = attacker.attr.HP * attack.power
 	else:
 		damage = attack.power
 
@@ -47,24 +47,31 @@ def CalculateDamage(attack, attacker, defender):
 		damage = damage * 1.5
 
 	if attack.defAttr == Attributes.STR:
-		damage = damage / defender.str
+		damage = damage / defender.attr.STR
 	elif attack.defAttr == Attributes.MSTR:
-		damage = damage / defender.mstr
+		damage = damage / defender.attr.MSTR
 	elif attack.defAttr == Attributes.DEF:
-		damage = damage / defender.DEF
+		damage = damage / defender.attr.DEF
 	elif attack.defAttr == Attributes.MDEF:
-		damage = damage / defender.mdef
+		damage = damage / defender.attr.MDEF
 	elif attack.defAttr == Attributes.SPD:
-		damage = damage / defender.spd
+		damage = damage / defender.attr.SPD
 	elif attack.defAttr == Attributes.ACC:
-		damage = damage / defender.acc
+		damage = damage / defender.ACC
 	elif attack.defAttr == Attributes.EVA:
-		damage = damage / defender.eva
+		damage = damage / defender.EVA
 	elif attack.defAttr == Attributes.HP:
-		damage = damage / defender.hp
+		damage = damage / defender.attr.HP
 
 	return damage
 
+# power (The attack power. This is a multiplier on most attacks. ex 1.5)
+# attackType (The type of the attack. Used for weaknesses and resistances)
+# offAttr (The attribute that is used for dealing damage. Usually STR or MSTR)
+# defAttr (The attribute used by the defender for blocking damage. Usually DEF or MDEF)
+# priority (The higher the priority, the faster the attack. Default is 1)
+# effect (A standard effect that the attack can inflict)
+# effpercent (The chance fo the effect to be inflicted.)
 class BasicAttack():
 	def __init__(self, power, attackType, offAttr, defAttr, priority, effect, effpercent):
 		self.power = power
--- Mons.py
+++ Mons.py
@@ -1,1 +1,81 @@
+#!/usr/bin/python
+from Standards import AutoNumber, Effects, Types, Attributes
+
+# HP (Hit Points)
+# STR (Strength)
+# DEF (Defense)
+# MSTR (Magic Strength)
+# MDEF (Magic Defense)
+# SPD (Speed)
+class Attributes():
+	#Attributes
+	def __init__(self, HP, STR, DEF, MSTR, MDEF, SPD):
+		self.HP = HP
+		self.STR = STR
+		self.DEF = DEF
+		self.MSTR = MSTR
+		self.MDEF = MDEF
+		self.SPD = SPD
+
+# attr (base values)
+#	HP
+#	STR
+#	DEF
+#	MSTR
+#	MDEF
+#	SPD
+# attack[level] (array on which each index references the level on which if can be accessed)
+#	attack1
+#	...
+#	attackN
+# typeOne (Mon type #1)
+# typeTwo (Mon type #2)
+class BasicMon():
+	def __init__(self, attrs, typeOne, typeTwo):
+		self.attr = attrs
+		self.typeOne = typeOne
+		self.typeTwo = typeTwo
+
+	def addAttack(self, attack, level):
+		self.attack[level] = attack
+
+# original (BasicMon Template)
+# 	attr
+#		HP
+#		STR
+#		DEF
+#		MSTR
+#		MDEF
+#		SPD
+#	attack[level]
+#		attack1
+#		...
+#		attackN
+#	typeOne (Mon type #1)
+#	typeTwo (Mon type #2)
+# level (current level)
+# cHP (Current HP)
+# EVA (Evasion)
+# ACC (Accuracy)
+# attackOne (First attack set for use in battle)
+# attackTwo (Second attack set for use in battle)
+# attackThree (Third attack set for use in battle)
+# attackFour (Fourth attack set for use in battle)
+class ActiveMon():
+	def __init__(self, prototype, level):
+		self.original = prototype
+		self.level = level
+		self.cHP = self.original.attr.HP * self.level * 2
+		self.EVA = 0
+		self.ACC = 100
+
+	def setAttacks(self, atkNum, attack):
+		if atkNum == 1:
+			self.attackOne = attack
+		elif atkNum == 2:
+			self.attackTwo = attack
+		elif atkNum == 3:
+			self.attackThree = attack
+		elif atkNum == 4:
+			self.attackFour = attack
 
